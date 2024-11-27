53094a58fde78933ff27903b56cf657a25b1c302
Hamza Nasir <101545759+hamzanasir47@users.noreply.github.com>
2024-11-27 20:52:20 +0500

    Update variables.tf

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,21 +1,25 @@
 variable "vnet_name" {
   description = "Name of the Virtual Network"
   type        = string
+  default     = "my-vnet"
 }
 
 variable "location" {
   description = "Azure region where the VNet will be created"
   type        = string
+  default     = "East US"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
+  default     = "myresourcegroup"
 }
 
 variable "address_space" {
   description = "Address space for the VNet"
   type        = list(string)
+  default     = "10.0.0.0/16"
 }
 
 variable "subnets" {
@@ -24,6 +28,7 @@ variable "subnets" {
     name                       = string
     address_prefixes           = list(string)
     private_endpoint_policies  = bool
+    default                    = "subnet-a"
   }))
 }
 
