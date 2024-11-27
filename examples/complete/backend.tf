20143de7c1b31a03ba51d003bd10bd18ac075d33
Hamza Nasir <101545759+hamzanasir47@users.noreply.github.com>
2024-11-27 15:05:50 +0500

    Update backend.tf

--- examples/complete/backend.tf
+++ examples/complete/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "myresourcegroup"
-    storage_account_name = "mytfstate1"
-    container_name       = "terraform-state"
-    region               =  "East US"
-    key                  = "prod.terraform.tfstate"
-  }
+  cloud {
+        # organization = "" 
+        # workspaces {
+        #     project = ""
+        #     name = ""
+        # }
+    }
 }
