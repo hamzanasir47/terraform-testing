37ff79a4001edcb2ae754e9311a5ef4ed7535844
Hamza Nasir <101545759+hamzanasir47@users.noreply.github.com>
2024-11-27 15:21:09 +0500

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,69 +1,34 @@
-resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-rg"
-  location = var.location
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  tags                = var.tags
 }
 
-resource "azurerm_subnet" "internal" {
-  depends_on           = [azurerm_virtual_network.main]
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "main" {
+  count                   = length(var.subnets)
+  name                    = var.subnets[count.index].name
+  resource_group_name     = var.resource_group_name
+  virtual_network_name    = azurerm_virtual_network.main.name
+  address_prefixes        = var.subnets[count.index].address_prefixes
+  enforce_private_link_endpoint_network_policies = var.subnets[count.index].private_endpoint_policies
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
+resource "azurerm_network_security_group" "main" {
+  count                = length(var.subnets)
+  name                 = "${var.subnets[count.index].name}-nsg"
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  tags                 = var.tags
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
+resource "azurerm_subnet_network_security_group_association" "main" {
+  count                = length(var.subnets)
+  subnet_id            = azurerm_subnet.main[count.index].id
+  network_security_group_id = azurerm_network_security_group.main[count.index].id
 }
